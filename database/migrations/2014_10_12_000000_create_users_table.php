38548b8cc1d52cc51b9462e5229a3730bbd2f7ad
waliur12 <sheikhwaliurrahman@gmail.com>
2023-08-19 12:24:55 +0600

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('account_type', ['Individual', 'Business']);
+            $table->double('balance');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
